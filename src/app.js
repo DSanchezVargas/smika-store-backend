c49e9abe91a62f2dd05c37846107ab9d634799fd
Tatsu <yeonbinjimin@gmail.com>
2026-05-29 01:13:39 -0500

    Aumentar límite de payload para imagenes de productos

--- src/app.js
+++ src/app.js
@@ -23,8 +23,25 @@ const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+
+app.use(
+  express.json({
+    limit: "25mb"
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "25mb"
+  })
+);
 
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
